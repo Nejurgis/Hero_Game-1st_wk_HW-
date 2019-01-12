c4769f24f3ecdf92d96f2747e59db054787271f7
quietQuiet <j.lietunovas@gmail.com>
2019-01-12 12:59:10 +0100

    added the hero object and some parameters

---
+++ YOUR_JAVASCRIPT_HERE.js
@@ -0,0 +1,12 @@
+// Write your JS here
+
+const hero = {};
+hero.name = 'frodo';
+hero.heroic = true;
+hero.inventory = [];
+hero.health = 10;
+hero.weapon = {
+    type: 'Golden Stick',
+    damage: 2,
+}
+
