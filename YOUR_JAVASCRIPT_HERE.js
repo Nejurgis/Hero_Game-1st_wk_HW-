2d3b218ca7b235931428f27d97bbddbe155cde95
quietQuiet <j.lietunovas@gmail.com>
2019-01-12 13:04:24 +0100

    adding the rest functionality

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -10,3 +10,30 @@ hero.weapon = {
     damage: 2,
 }
 
+// creating a handler that reaches an existing element in the DOM
+const container = document.getElementById('container');
+
+// INN
+// create an element inn
+const inn = document.createElement('IMG');
+// assign it an id
+inn.id = 'inn';
+// append the inn to the container 
+container.append(inn);
+// create a event handler that waits for the click event
+inn.addEventListener('click', function() { rest(hero)});
+
+
+
+function rest(hero) {
+    hero.health = 10;
+    return hero;
+}
+
+function pickUpItem() {
+
+}
+
+function equipWeapon() {
+    
+}
