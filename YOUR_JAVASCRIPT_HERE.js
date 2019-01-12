92ebbb73b5523db541000c6925836abc12c8e0e8
quietQuiet <j.lietunovas@gmail.com>
2019-01-12 13:30:52 +0100

    added functionality to show hero statistics

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,7 +1,7 @@
 // Write your JS here
 
 const hero = {};
-hero.name = 'frodo';
+hero.name = 'Frodo';
 hero.heroic = true;
 hero.inventory = [];
 hero.health = 10;
@@ -62,3 +62,30 @@ function equipWeapon(hero){
 }
 
 
+// ## Section 4 - Bonus: Show me what you got
+
+
+
+// - Create a form that allow users to change the name of their hero.
+
+// - Call `displayStats` when some data of the hero has change to update the page.
+
+// - Implement fighting an enemy. (how you do it is up to you!)
+
+// - When an enemy or weapon gets clicked it gets deleted from the page
+
+
+
+
+function displayStats() {
+    const details = document.createElement('p');
+    console.log(details);
+    details.innerText = `Hero Name: ${hero.name} \n Hero Health: ${hero.health}\n Hero Weapon Type: ${hero.weapon.type}\n Hero Weapon Damage: ${hero.weapon.damage} `;
+    container.append(details);
+
+}
+displayStats();
+
+const button = document.createElement('button');
+button.innerText = 'I am a button!'
+container.append(button);
